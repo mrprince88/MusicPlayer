5ec84a45399aba4815161a71efbb499fd712be68
mrprince88 <mrprince0088@gmail.com>
2024-02-13 13:20:57 +0530

    chore: remove context

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,8 @@
 import { Stack } from "expo-router";
-import AudioProvider from "src/contexts/Audio";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import "src/styles/global.css";
 
 export default function layout() {
   return (
-    // <AudioProvider>
     <Stack>
       <Stack.Screen
         name="index"
@@ -21,6 +18,5 @@ export default function layout() {
         }}
       />
     </Stack>
-    // </AudioProvider>
   );
 }
